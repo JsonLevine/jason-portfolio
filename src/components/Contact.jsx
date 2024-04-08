1d90fd5dce18baa0666378a057ce5778adfdcdfd
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2024-04-08 11:04:37 -0400

    Updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
 
 function Contact() {
 const [name, setName] = React.useState("");
@@ -66,6 +67,7 @@ function encode(data) {
               name="contact"
               onSubmit={handleSubmit}
               className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+              <ChatBubbleLeftRightIcon className="w-8 text-gray-500 mb-4" />
               <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                 Lets connect!
               </h2>
@@ -112,6 +114,7 @@ function encode(data) {
               </div>
               <button
                 type="submit"
+                disabled="true"
                 className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 Submit
               </button>
