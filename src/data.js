25a6a5a92bb78060af33ba8428587c7651f21b3b
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2024-06-26 22:05:44 -0400

    Update headshots

--- src/data.js
+++ src/data.js
@@ -22,7 +22,7 @@ export const testimonials = [
   {
     quote:
       "\"Working with Jason was always a pleasure. He consistently demonstrated professionalism, reliability, and a positive attitude. His problem-solving skills and creativity were truly impressive, making him a valuable asset to the team. I feel fortunate to have worked alongside such a talented individual.\"",
-    image: "https://media.licdn.com/dms/image/C4E03AQHpw-lgzMKTbw/profile-displayphoto-shrink_800_800/0/1519673938976?e=1718841600&v=beta&t=Dqlcjev_vdqpvBazp07yfHy-0fLO3eufVvB0No_er8s",
+    image: "./assets/Chris_headshot.jpg",
     name: "Christopher Cook",
     company: "Chewy",
     title: "Senior Software Engineer"
@@ -30,7 +30,7 @@ export const testimonials = [
   {
     quote:
       "\"It was a genuine pleasure to work with Jason, whom I had the privilege of directly managing. As a seasoned UI developer, Jason took on the role of experimentation lead for our analytics team, showcasing his adeptness in steering medium to large-sized projects with remarkable independence. His expertise shone brightly as he efficiently addressed and resolved any experimentation-related queries, earning him the reputation of a subject matter expert. Jason’s proficiency in React is outstanding, and his ability to dive deep into complex problems sets him apart. Above all, his quick learning ability and collaborative spirit make him an invaluable asset to any team.\"",
-    image: "https://media.licdn.com/dms/image/D5603AQGmBxX79rwLxw/profile-displayphoto-shrink_800_800/0/1704249763162?e=1718841600&v=beta&t=Pi0qiE8Q9Gh2blAvY1IdUY6UjP2NsnvxbgkyCrUkDls",
+    image: "./assets/Aditya_headshot.jpg",
     name: "Aditya Jain",
     company: "Chewy",
     title: "Associate Director"
