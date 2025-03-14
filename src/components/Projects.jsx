0b7e69bd4951646a0c6e24ee8e39d10b179c2ad5
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-03-14 15:14:56 -0400

    Lots more stuff

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,8 @@
 import { React } from 'react'
-import { CubeTransparentIcon } from "@heroicons/react/24/solid";
+import { CubeTransparentIcon, IdentificationIcon } from "@heroicons/react/24/solid";
 import IconGitHub from './icons/github'
 import { projects as actualProjects } from "../data";
+import Accordion from "./Accordion.jsx"
 
 function Projects({overrideProjects = null}) {
 
@@ -12,18 +13,51 @@ function Projects({overrideProjects = null}) {
           <div className="container xl:px-40 px-10 py-10 mx-auto text-center">
             <div className="flex flex-col w-full mb-10">
               <div data-testid="section-title">
-                <CubeTransparentIcon className="hover:animate-spin-slow mx-auto inline-block w-10 mb-4" />
+                <IdentificationIcon className="mx-auto inline-block w-10 mb-4" />
                 <h1 className="jersey-25 sm:text-5xl text-4xl font-medium title-font mb-4 text-stronghold-platinum">
                   Professional Experience
                 </h1>
               </div>
-              <p className=" pb-4 mx-auto leading-relaxed text-base" data-testid="section-description">
-              With over five years of experience as a frontend developer, I have honed my skills across a variety of high-visibility projects.
-              At Chewy, my responsibilities included developing and enhancing critical pages such as the <a className="text-indigo-400" href="https://www.chewy.com/b/food-332">Search/Browse</a>, <a className="text-indigo-400" href="https://www.chewy.com/american-journey-protein-grains/dp/136721">Product Detail Page (PDP)</a>,
-              and <a className="text-indigo-400" href="https://www.chewy.com/">home</a> pages. Additionally, I specialized as a frontend analytics developer, collaborating extensively with multiple teams to gather
-              user insights and conduct robust A/B testing using Optimizely.
+              <p className="mb-10 pb-4 mx-auto leading-relaxed text-base" data-testid="section-description">
+              Recently, I have been making the transition to Project Management/Technical Project Management.
+              Prior to that, I worked for over 7 years as a frontend-leaning fullstack software engineer.
+              Expand the sections below for some highlites of my career so far.
               </p>
-              <p className=" mx-auto leading-relaxed text-base pt-4 " data-testid="section-description-2">
+              <div className= "flex flex-col lg:flex-row justify-between" data-testid="jobs-list">
+                <div className="lg:w-1/2 p-4">
+                  <Accordion title="Project Management">
+                    <ul>
+                      <li><strong className="jersey text-2xl text-stronghold-imperial-red">Currently</strong>, I am working toward a Certified Associate in Project Management (CAPM) certification with <a className="text-indigo-400" href="https://www.pmi.org">pmi.org</a>. Estimated completion 4/1/2025.</li>
+                      <li className="mt-2 border-t-2 border-stronghold-imperial-red jersey text-2xl text-blue-500">Chewy:</li>
+                      <li> •	At Chewy, I managed a cross-team backlog of tickets in JIRA and contributed heavily to analytics document creation & organization within Confluence</li>
+                      <li> •	In addition, I organized regular product demos, provided frequent updates to stakeholders, and coordinated/ran regular agile ceremonies for the analytics team in order to streamline feature development</li>
+                      <li className="mt-2 border-t-2 border-blue-500 jersey text-2xl text-orange-500">Optum:</li>
+                      <li> •	At Optum, I volunteered as a manager for new hires in our technology development program. </li>
+                      <li> •	In addition, I was an active member of the Optum branch of Toastmasters International (an educational organization that teaches public speaking and leadership skills) </li>
+                    </ul>
+                  </Accordion>
+                </div>
+                <div className="lg:w-1/2 p-4" >
+                  <Accordion title="Frontend Development">
+                    <ul>
+                      <li><strong className="jersey text-2xl text-stronghold-imperial-red">Currently</strong>, I am working on developing an online React course in partnership with newline.co.  I am also continuously updating this site, and taking courses to keep my frontend skills up to date</li>
+                      <li className="mt-2 border-t-2 border-stronghold-imperial-red jersey text-2xl text-blue-500">Chewy:</li>
+                      <li>  •	At Chewy, my responsibilities included developing and enhancing critical pages such as the <a className="text-indigo-400" href="https://www.chewy.com/b/food-332">Search/Browse</a>, <a className="text-indigo-400" href="https://www.chewy.com/american-journey-protein-grains/dp/136721">Product Detail Page (PDP)</a>,
+                        and <a className="text-indigo-400" href="https://www.chewy.com/">home</a> pages. Additionally, I specialized as a frontend analytics developer, collaborating extensively with multiple teams to gather
+                        user insights and conduct robust A/B testing using Optimizely.</li>
+                      <li className="mt-2 border-t-2 border-blue-500 jersey text-2xl text-orange-500">Optum:</li>
+                      <li>  •	At Optum, I led the continuous planning and implementation of new features for Optum's internal placement tool.  I frequently held live user-testing sessions with stakeholders to gather feedback.</li>
+                    </ul>
+                  </Accordion>
+                </div>
+              </div>
+              <div data-testid="section-title">
+                <CubeTransparentIcon className="hover:animate-spin-slow mx-auto inline-block w-10 mb-4 mt-20" />
+                <h1 className="jersey-25 sm:text-5xl text-4xl font-medium title-font mb-4 text-stronghold-platinum">
+                  Side Projects
+                </h1>
+              </div>
+              <p className="mx-auto leading-relaxed text-base pt-4 " data-testid="section-description-2">
               When I’m not working, I love exploring new skills and experimenting with different projects. 
               I’ve built apps with React, crafted dynamic websites on WordPress, and also dabbled in graphic and logo design with Photoshop and Illustrator.
                Below is a mix of my work that shows my range and passion for digital design.
@@ -36,7 +70,7 @@ function Projects({overrideProjects = null}) {
                   rel="noreferrer"
                   href={project.link}
                   key={project.image}
-                  className="sm:w-1/2  p-4"
+                  className="sm:w-1/2 p-4"
                   data-testid="project-item"
                 >
                   <div className="flex rounded border-2 border-gray-600 relative transition ease-in-out duration-300 hover:shadow-project-shadow">
