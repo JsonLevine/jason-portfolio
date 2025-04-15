83d5b06280386b3267056cc27909545fcb610c23
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-04-15 15:33:02 -0400

    Sidebar updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,9 +14,9 @@ function Sidebar() {
         <a className="w-12" href="https://www.instagram.com/adjacent_levine/" target="_blank" rel="noreferrer"><IconInstagram/></a>
         <span className="w-[.1rem] block h-40 bg-stronghold-imperial-red"></span>
       </div>
-      <div className="hidden md:flex lg:ml-4 fixed flex-col items-center justify-center w-12 right-0 top-12">
-          <span className="w-[.1rem] block h-40 bg-stronghold-imperial-red"></span>
-          <a href="mailto:jason.levine131@gmail.com" className="transition ease-in-out duration-300 hover:text-stronghold-imperial-red hover:translate-y-1 pl-[12rem] rotate-90">jason.levine131@gmail.com</a>
+      <div className="hidden md:flex lg:ml-4 fixed flex-col items-center justify-center w-12 right-0 top-0">
+          <span className="w-[.1rem] block h-60 bg-stronghold-imperial-red"></span>
+          <a href="mailto:jason.levine131@gmail.com" className="transition ease-in-out duration-300 hover:text-stronghold-imperial-red hover:translate-y-1 mt-[6rem] rotate-90">jason.levine131@gmail.com</a>
       </div>
 
       {/* Place on bottom otherwise */}
