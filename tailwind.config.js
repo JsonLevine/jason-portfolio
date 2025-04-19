a04347057550f01f6a2acdb1bbc00c6741408105
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-04-19 19:54:25 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,6 @@ module.exports = {
           '100%':{opacity: '1'}
         }
       },
-      // colors: {
-      //   'stronghold-red': '#E64747',
-      //   'stronghold-eerie-black': '#191919',
-      //   'stronghold-onyx': '#404040',
-      //   'stronghold-platinum': '#E4E4E4',
-      //   'stronghold-jet': '#343434',
-      //   'stronghold-white': '#ffffff',
-      //   'stronghold-green': '#4ADE80',
-      //   'chewy-blue': '#3b82f6',
-      //   'optum-orange': '#f97316',
-      //   'link-blue': '#818cf8',
-      //   'main-text': '#9CA3AF'
-      // },
       colors: {
         'stronghold-red': '#E64747',
         'stronghold-eerie-black': '#191919',
