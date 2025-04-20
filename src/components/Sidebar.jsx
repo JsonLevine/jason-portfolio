47f63c281b5e4ee74319ac9fbbc86944fc74af4c
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-04-20 16:53:40 -0400

    Make navbar fixed so you cant scroll past it

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ function Sidebar() {
         <a className="w-12" href="https://www.instagram.com/adjacent_levine/" target="_blank" rel="noreferrer"><IconInstagram/></a>
         <span className="w-[.1rem] block h-40 bg-stronghold-red"></span>
       </div>
-      <div className="hidden md:flex lg:ml-4 fixed flex-col items-center justify-center w-12 right-0 top-0">
+      <div className="hidden md:flex lg:ml-4 fixed flex-col items-center justify-center w-12 right-0 top-0 z-20">
           <span className="w-[.1rem] block h-60 bg-stronghold-red"></span>
           <a href="mailto:jason.levine131@gmail.com" className="transition ease-in-out duration-300 hover:text-stronghold-red hover:translate-y-1 mt-[6rem] rotate-90">jason.levine131@gmail.com</a>
       </div>
