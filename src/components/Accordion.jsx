5650571e8864c3551bfa2a5943fe586bc96bee30
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-04-21 17:31:05 -0400

    Extra updates (non-header)

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -7,7 +7,7 @@ function Accordion({ title, children }) {
   return (
     <div className="group border-stronghold-jet rounded ">
       <button
-        className="rounded transition ease-in-out duration-300 bg-stronghold-jet group-hover:bg-stronghold-red hover:-translate-y-1 hover:scale-110 flex items-center justify-between w-full p-4"
+        className={`hover:rounded ${ isOpen ? 'rounded-t' : 'rounded'} transition ease-in-out duration-300 bg-stronghold-jet group-hover:bg-stronghold-red hover:-translate-y-1 hover:scale-110 flex items-center justify-between w-full p-4`}
         onClick={() => setIsOpen(!isOpen)}
         aria-expanded={isOpen}
       >
@@ -46,7 +46,7 @@ function Accordion({ title, children }) {
         </svg>
       </button>
       <div
-        className={`group-hover:border-stronghold-red border-stronghold-onyx rounded-bl rounded-br border-b-2 border-l-2 border-r-2 max-h-full bg-stronghold-eerie-black bg-opacity-70 text-stronghold-platinum overflow-hidden transition ease-in-out duration-300 ${
+        className={`group-hover:border-stronghold-red border-stronghold-onyx rounded-bl rounded-br border-b-2 border-l-2 border-r-2 max-h-full bg-stronghold-jet bg-opacity-80 text-stronghold-platinum overflow-hidden transition ease-in-out duration-300 ${
           isOpen ? 'visible' : 'hidden'
         }`}
       >
