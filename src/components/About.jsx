2dcbce3fff770dad76381628b0f27a1046ad6deb
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-04-23 18:48:36 -0400

    About tests

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,6 +26,7 @@ function About() {
           <div className="flex flex-col xl:flex-row justify-center mb-4">
             <div className="flex justify-center">
               <a
+                data-testid="connect-with-me-button"
                 onClick={() => scrollToSectionBottom("contact")}
                 tabIndex='0'
                 className="group cursor-pointer transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110 jersey text-2xl inline-flex bg-stronghold-jet border-0 py-2 px-6 focus:outline-none focus:bg-stronghold-red hover:bg-stronghold-red text-stronghold-platinum rounded">
