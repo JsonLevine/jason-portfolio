db66620609c0256ca6baf0241a9ea0f4858e882e
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-07-09 17:35:12 -0400

    Update ReducedMotionBanner.jsx

--- src/components/ReducedMotionBanner.jsx
+++ src/components/ReducedMotionBanner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ReducedMotionBanner() {
   return (
     <div className='text-center text-2xl bg-stronghold-onyx p-4'>
-        <span className='text-stronghold-platinum'>You are currently viewing the site with reduced motion enabled</span>
+        <span className='text-stronghold-platinum'>You are currently viewing the site with <strong>reduced motion</strong> enabled</span>
     </div>
   )
 }
