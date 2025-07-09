7fa179aee90c8ff38c36a3423f6c193aed87f237
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-07-09 16:39:28 -0400

    Remove hover events for URM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useReducedMotion } from '@react-spring/web'
 import { loadFull } from "tsparticles";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -15,6 +16,7 @@ import "./App.css"
 
 export default function App() {
   const [init, setInit] = useState(false);
+  const reducedMotion = useReducedMotion()
 
   // Initialize the particles engine (the background animation)
   useEffect(() => {
@@ -31,17 +33,18 @@ export default function App() {
 
   return (
     <main className='text-main-text background body-font'>
-      {init && <Particles options={particlesOptions}/>}
+      {init && !reducedMotion && <Particles options={particlesOptions}/>}
       {/* Uncomment the line below (and the import on line 12) to enable the screen debugger */}
       {/* <ScreenDebugger />  */}
       <Navbar />
+      {reducedMotion ? <p>You're using reduced motion!</p> : null}
       <About />
       <Projects />
       <Skills />
       <Testimonials />
       <Contact />
       <Footer />
-      <Sidebar />
+      <Sidebar useReducedMotion/>
     </main>
   );
 }
