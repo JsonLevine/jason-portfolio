f56f121e3d695d86aea7238e62b18785cfd00dd3
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-08-10 17:25:41 -0400

    Update PageNotFound.jsx

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -9,7 +9,6 @@ function PageNotFound() {
     <div className="flex flex-col items-center justify-center min-h-screen">
 			<img src="/assets/Shocked_Ron.gif" alt="" className="w-1/2 lg:w-1/3 h-1/2 object-cover" />
 			<h1 className="text-stronghold-red text-center my-10 text-3xl">404 - Page Not Found</h1>
-
 			<button
 				onClick={() => navigate('/')}
 				className="group cursor-pointer transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110 jersey text-2xl inline-flex bg-stronghold-jet border-0 py-2 px-6 focus:outline-none focus:bg-stronghold-red hover:bg-stronghold-red text-stronghold-platinum rounded"
